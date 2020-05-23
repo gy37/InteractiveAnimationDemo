febd48c3d5eaa86201832db444c27bf0d2f85dec
gy37 <805331263@qq.com>
2020-05-23 19:15:32 +0800

    滚动菜单栏贴顶增加注释，和一些改动

--- pages/stick-top/stick-top.js
+++ pages/stick-top/stick-top.js
@@ -3,7 +3,8 @@ Page({
   data: {
     sticky: false,
     opacity: 0,
-    height: height
+    height: height,
+    titles: ["首页", "活动", "菜单", "我的"]
   },
   //滚动条监听
   onPageScroll: function (e) {
@@ -13,5 +14,12 @@ Page({
     
   },
   onReady() {
+  },
+  onTapTitle: function (e) {
+    //点击view中text时，target的dataset中为空，currentTarget的dataset中有值
+    //点击view其他位置时，target和currentTarget的dataset中都有值
+    //target触发事件的组件的一些属性值集合；currentTarget当前组件的一些属性值集合，事件绑定的当前组件
+    console.log(e.currentTarget.dataset.item);
+    console.log(e.target.dataset.item);
   }
 })
